a690660cabe525399517a475df7a819f587eb741
Senthil Ramasamy <www.senthilr.com@gmail.com>
2015-12-13 08:10:46 +0530

    front end

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,7 @@
 class VisitorsController < ApplicationController
-	@probs = Prob.order(votes: :desc).limit(6)
+	def index
+	@problems = Prob.order(votes: :desc).limit(6)
 	@solns = Soln.order(created_at: :desc).limit(6)
-	@Challenge = Challenge.order(created_at: :desc).limit(6)
+	@challenges = Challenge.order(created_at: :desc).limit(6)
+end
 end
